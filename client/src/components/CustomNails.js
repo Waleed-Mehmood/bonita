9b9f7a62f35950a70207183fbcd3e67eb3c57c77
Wajid Ali Khan <88149763+wajidweb@users.noreply.github.com>
2024-12-26 17:00:45 +0500

    few changes

--- client/src/components/CustomNails.js
+++ client/src/components/CustomNails.js
@@ -6,7 +6,7 @@ const CustomNails = () => {
     <section className="py-5">
       <div className="container">
         <div className="row align-items-center">
-          <div className="col-12 col-lg-5 offset-1 text-center text-lg-start mt-4 mt-lg-0">
+          <div className="col-12 col-lg-6 text-center text-lg-start mt-4 mt-lg-0 mb-3">
             <h2
               className="mb-4"
               style={{ color: "#ff8ebf", fontWeight: "bold" }}
